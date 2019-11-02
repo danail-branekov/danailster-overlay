28b334bac9e54bff09f9f7dbe6cd26de009489c3
Danail Branekov <danailster@gmail.com>
2019-11-02 18:49:30 +0200

    Script to automate bumping viber in the ebuild

---
+++ net-im/viber/bump-viber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Viber is a piece of software with quite a crappy delivery process which only provides latest linux binary. 
+# This makes it quite hard for Linux distribution package maintainers to keep up with releasing new versions as bumping to a new version has to be done manually
+
+
+set -x
+
+emerge --unmerge viber
+
+rm -rf /usr/portage/distfiles/viber-9999.deb
+rm -rf /usr/portage/distfiles/viber-9999.deb
+
+ebuild viber-9999.ebuild manifest clean merge
